99e699aff7e3dedafae95564e59a0cb26debe55d
suxiaohutot <1609909764@qq.com>
2022-04-15 10:17:14 +0800

    1

---
+++ .history/components/Footer_20220415100303.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+  return (
+    <>
+      <footer className={styles.footer}>
+        With Me for you
+      </footer>
+    </>
+  )
+}
--- .history/pages/index_20220414182856.js
+++ .history/pages/index_20220414182856.js
@@ -7,11 +7,11 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>大猫的友人帐</title>
-        <link rel="icon" href="/favicon1.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="欢迎来到大猫的友人帐!" />
+        <Header title="欢迎来到我的世界!" />
         <p className="description">
           想吃点啥就吃点啥
         </p>
